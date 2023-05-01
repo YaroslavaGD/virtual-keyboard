aefbbe45e31859e33b60fd571ff67fd5d7fe2d4e
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-05-02 01:29:11 +0200

    refactor: delete commented code

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './index.html';
 import './index.scss';
 import keyboardFunctions from './modules/keyboard';
 
+alert('Уважаемые проверяющие! Если есть возможность, проверьте мою работу ближе к делайну. Хочу доделать. Спасибо!');
 keyboardFunctions();
