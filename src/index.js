9a5d006a5c5b2aafd916da18b88bcb1bd2d25f51
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-05-01 13:49:34 +0200

    feat: implement main styles for keyboard

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import './index.html';
 import './index.scss';
-import code from './img/keyboard.svg';
 import { mult, sum } from './modules/calc';
-
-const imgWrap = document.querySelector('.img');
-const img = new Image();
-img.src = code;
-
-imgWrap.append(img);
 
 mult(2, 4);
 sum(2, 4);
